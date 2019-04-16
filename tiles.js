c61b7a6ec23cf06624029ee12be0f80eb468442f
sentibot <sentibotlite@gmail.com>
2019-04-16 11:10:35 +0300

    Added partials

--- tiles.js
+++ tiles.js
@@ -1,12 +1,12 @@
 var express = require('express');
-var qdb = require('./lib/quotes.js');
+var ldb = require('./lib/ldb.js');
 
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
 var handlebars = require('express3-handlebars')
-    .create({ defaultLayout: 'main' });
+    .create({ defaultLayout: 'main', partialsDir:['views/partials/'] });
 
 // configure express to use the handlebars view engine as default
 app.engine('handlebars', handlebars.engine);
@@ -15,25 +15,61 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static(__dirname + '/public'));
 
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
     res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    next();
+});
+
+function getTiles() {
+    return {
+        tdata: [
+            {
+                name: 'Victoria Grey',
+                tileUrl: 'https://www.tileshop.com/products/victoria-grey-rouen-stone-mosaic-wall-tile-616152?g2=material&c=marble&sc=wall',
+                iconUrl: 'https://s7d1.scene7.com/is/image/TileShop/616148?$Product_Search$',
+                tlength: '40',
+                twidth: '20'
+            },
+            {
+                name: 'Fressia Toros',
+                tileUrl: 'https://www.tileshop.com/products/fressia-toros-black-marble-wall-tile-7-x-18-in-650341?g2=material&c=marble&sc=wall',
+                iconUrl: 'https://s7d1.scene7.com/is/image/TileShop/650341?$Product_Search$',
+                tlength: '50',
+                twidth: '25'
+            },
+            {
+                name: 'Vinica Mugla',
+                tileUrl: 'https://www.tileshop.com/products/vinica-mugla-white-marble-architectural-wall-tile-12-in-650340?g2=material&c=marble&sc=wall',
+                iconUrl: 'https://s7d1.scene7.com/is/image/TileShop/650340?$Product_Search$',
+                tlength: '35',
+                twidth: '15'
+            }
+        ]
+    };
+}
+
+app.use(function (req, res, next) {
+    if (!res.locals.partials) {
+        res.locals.partials = {};
+    }
+    res.locals.partials.tiles = getTiles();
     next();
 });
 
 // setup routes
 app.get('/', function (req, res) {
-    res.render('home', { quote: qdb.getQuote() });
+    res.render('home', { quote: ldb.getQuote() });
 });
 
 app.get('/about', function (req, res) {
-    res.render('about', { pageTestScript: '/qa/tests-about.js' });    
+    res.render('about', { pageTestScript: '/qa/tests-about.js' });
 });
 
-app.get('/inspirations/virtualroom', function(req, res){
+app.get('/inspirations/virtualroom', function (req, res) {
     res.render('inspirations/virtualroom', { pageTestScript: '/../qa/tests-crosspage.js' });
 });
 
-app.get('/inspirations/request-rate', function(req, res){
+app.get('/inspirations/request-rate', function (req, res) {
     res.render('inspirations/request-rate');
 });
 

fdf4007caa52d68c162cba56747f8238c55ddaa1
sentibot <sentibotlite@gmail.com>
2019-04-18 00:59:53 +0300

    Handle uncaught exceptions with domains

--- tiles.js
+++ tiles.js
@@ -10,6 +10,55 @@ var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
+// [******************** Global Error Handling ********************] 
+
+app.use(function(req, res, next){
+    // create domain for incoming request
+    var domain = require('domain').create();
+
+    // add error event to this domain
+    domain.on('error', function(err){
+        console.log('DOMAIN ERROR:\n', err.stack);
+        try {
+            // shotdown process in 5s
+            setTimeout(() => {
+                console.error('Shuting down...')
+                process.exit(1);
+            }, 5000);
+
+            // disconnect worker from cluster
+            var worker = require('cluster').worker;
+            if(worker){
+                worker.disconnect();
+            }
+
+            // stop taking new requests
+            server.close();
+            try {
+                // try to use express error route
+                next(err);
+            } catch (error) {
+                // if it's not working try plain Node response
+                console.log('Express error mechanism failed: \n', err.stack);
+                res.status.code = 500;
+                res.setHeader('content-type', 'text/plain');
+                res.end('Server error');
+            }
+        } catch (error) {
+            console.error('Unable to send 500 response', err.stack);
+        }
+    });
+
+    // add objects to the domain
+    domain.add(req);
+    domain.add(res);
+
+    // execute the rest of the request
+    domain.run(next);
+});
+
+// [******************** Global Error Handling ********************] 
+
 var handlebars = require('express3-handlebars')
     .create({ defaultLayout: 'main', partialsDir: ['views/partials/'] });
 
@@ -207,8 +256,9 @@ app.use(function (error, req, res, next) {
     res.render('500');
 });
 
+var server;
 function startServer() {
-    http.createServer(app).listen(app.get('port'), function () {
+    server = http.createServer(app).listen(app.get('port'), function () {
         console.log('Express started in ' + app.get('env') + ' mode on http://localhost:' + app.get('port') + '; Press Ctrl-C to terminate.');
     });
 }
